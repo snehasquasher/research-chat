c06768fb59ccbe8bf4cc8f09dc3590cd3e304366
nijhawananushka <55636931+nijhawananushka@users.noreply.github.com>
2023-11-22 13:48:07 -0500

    refactoring utils into py

---
+++ utils/pinecone.py
@@ -0,0 +1,29 @@
+import pinecone
+import os
+
+def get_matches_from_embeddings(embeddings, top_k, namespace = ''):
+    pinecone.init(
+        api_key=os.getenv("PINECONE_API_KEY"), 
+        environment=os.getenv("PINECONE_ENVIRONMENT"))
+    
+    index_name = os.getenv("PINECONE_INDEX") if os.getenv("PINECONE_INDEX") else ""
+    if index_name == "":
+        raise Exception("PINECONE_INDEX environment variable not set")
+    
+    indexes = pinecone.list_indexes()
+    if index_name not in indexes:
+        raise ValueError("Index ${index_name} does not exist")
+
+    index = pinecone.Index(index_name=index_name)
+
+    try:
+        query_result = await index.query(
+            namespace=namespace,
+            top_k=top_k,
+            vector=embeddings,
+            include_metadata=True
+        )
+        return query_result
+    except Exception as e:
+        console.log("Error querying embeddings: ", e)
+        raise Exception("Error querying embeddings: ${e}")
