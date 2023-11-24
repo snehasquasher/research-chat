e4e5abc8b9fa571eedd67f406903da9e9553cbac
Sneha <sneha.squasher@gmail.com>
2023-11-23 22:47:12 -0500

    documentation

--- api/index.py
+++ api/index.py
@@ -13,6 +13,47 @@ app = Flask(__name__)
 
 @app.route("/api/chat", methods = ["POST"])
 async def chat():
+    """
+    This endpoint handles POST requests for generating responses using the OpenAI API based on user messages.
+
+    Request Body:
+    -------------
+    - messages (array): An array of message objects. Each object should have 'role' (user/system) and 'content'.
+    - file_name (string): A file name that is used for fetching context relevant to the conversation.
+
+    Process:
+    --------
+    1. Initializes the OpenAI client using the API key.
+    2. Extracts 'messages' and 'file_name' from the request's JSON body.
+    3. Constructs the conversation context and prepares the prompt for the OpenAI API.
+    4. Calls the OpenAI API's chat completions method with the constructed prompt to generate a response.
+
+    Response:
+    ---------
+    - Success: Returns a JSON response containing the AI-generated message content.
+    - Failure: Returns a JSON response with the error message and a 500 status code.
+
+    Note:
+    -----
+    The 'messages' array must include the conversation history, where the last message is the user's latest input. 
+    The 'file_name' is used to fetch additional context for the conversation.
+
+    Example Usage:
+    --------------
+    ```
+    POST /api/chat HTTP/1.1
+    Host: <your_api_url>
+    Content-Type: application/json
+
+    {
+        "messages": [
+            {"role": "user", "content": "Hello, AI!"},
+            ...
+        ],
+        "file_name": "context_file.txt"
+    }
+    ```
+    """
     try:
         client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
         data = request.json
@@ -23,7 +64,7 @@ async def chat():
         prompt = [
             {
                 "role": "system",
-                "content": f"""AI assistant is a brand new, powerful, human-like artificial intelligence.
+                "content": """AI assistant is a brand new, powerful, human-like artificial intelligence.
                     The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
                     AI is a well-behaved and well-mannered individual.
                     AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
@@ -39,11 +80,13 @@ async def chat():
                     """,
             },
         ]
+
         user_messages = [message for message in messages if message['role'] == 'user']
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages= prompt + user_messages
         )
+
         return jsonify(response.choices[0].message.content)
 
     except Exception as e:
@@ -51,6 +94,31 @@ async def chat():
 
 @app.route("/api/clear_index", methods = ["POST"])
 def clear_index():
+    """
+    This endpoint handles POST requests for deleting a specific Pinecone index.
+
+    Process:
+    --------
+    1. Initializes Pinecone using the API key and environment variable.
+    2. Retrieves the name of the Pinecone index from the environment variable.
+    3. Deletes the specified Pinecone index.
+
+    Response:
+    ---------
+    - Success: Returns a JSON response confirming successful deletion of the index.
+    - Failure: Returns a JSON response with the error message and a 500 status code.
+
+    Note:
+    -----
+    Ensure that the Pinecone API key and index name are correctly set in the environment variables.
+
+    Example Usage:
+    --------------
+    ```
+    POST /api/clear_index HTTP/1.1
+    Host: <your_api_url>
+    ```
+    """
     try:
         pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT"))
         index = pinecone.Index(index_name = os.getenv("PINECONE_INDEX"))
@@ -62,6 +130,33 @@ def clear_index():
 
 @app.route("/api/generate_embeddings", methods = ["POST"])
 async def generate_embeddings():
+    """
+    This endpoint processes a POST request to generate embeddings from a provided PDF file.
+
+    Request Body:
+    -------------
+    The request must include a 'pdf' field in the form-data, containing the PDF file from which embeddings will be generated.
+
+    Process:
+    --------
+    1. The function checks if a 'pdf' file is included in the request. If not, it returns a 400 status code with an error message.
+    2. If a PDF file is present, it calls the `upload_and_generate_embedding` function, passing the PDF file and an environment variable "PINECONE_INDEX".
+    3. The `upload_and_generate_embedding` function is responsible for handling the PDF file, generating embeddings, and potentially interacting with the Pinecone service or similar.
+
+    Response:
+    ---------
+    - Success: Returns a JSON response containing the generated embeddings.
+    - Failure: If any exception occurs, the function returns a JSON response with the error message and a 500 status code.
+
+    Note:
+    -----
+    Ensure that the 'pdf' file is provided in the correct format and that the "PINECONE_INDEX" environment variable is properly set for the function to execute successfully.
+    
+    Example Usage:
+    --------------
+    curl -X POST -F 'pdf=@path_to_pdf_file.pdf' http://<your_api_url>/api/generate_embeddings
+
+    """
     try:
         if 'pdf' not in request.files:
             return 'No PDF file provided', 400
