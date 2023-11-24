e0ee2bc3cf8fdfe678d6696e02b0d6582c8c5a53
aliceao <alice.ao1950@gmail.com>
2023-11-24 13:00:09 -0500

    small chat changes

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -7,9 +7,9 @@ import ReactMarkdown from "react-markdown";
 import Textarea from "react-textarea-autosize";
 
 const examples = [
-  "Get me the top 5 stories on Hacker News in markdown table format. Use columns like title, link, score, and comments.",
-  "Summarize the comments in the top hacker news story.",
-  "What is the top story on Hacker News right now?",
+  "Compare and contrast the abstracts of the documents I uploaded.",
+  "Summarize the comments of my first document.",
+  "What are some common limitations found in these research papers?",
 ];
 
 export default function Chat() {
@@ -87,47 +87,9 @@ export default function Chat() {
         <div className="border-gray-200sm:mx-0 mx-5 mt-20 max-w-screen-md rounded-md border sm:w-full">
           <div className="flex flex-col space-y-4 p-7 sm:p-10">
             <h1 className="text-lg font-semibold text-black">
-              Welcome to ChatHN!
+              Chat with your documents now!
             </h1>
-            <p className="text-gray-500">
-              This is an{" "}
-              <a
-                href="https://github.com/steven-tey/chathn"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-medium underline underline-offset-4 transition-colors hover:text-black"
-              >
-                open-source
-              </a>{" "}
-              AI chatbot that uses{" "}
-              <a
-                href="https://platform.openai.com/docs/guides/gpt/function-calling"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-medium underline underline-offset-4 transition-colors hover:text-black"
-              >
-                OpenAI Functions
-              </a>{" "}
-              and{" "}
-              <a
-                href="https://sdk.vercel.ai/docs"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-medium underline underline-offset-4 transition-colors hover:text-black"
-              >
-                Vercel AI SDK
-              </a>{" "}
-              to interact with the{" "}
-              <a
-                href="https://github.com/HackerNews/API"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="font-medium underline underline-offset-4 transition-colors hover:text-black"
-              >
-                Hacker News API
-              </a>{" "}
-              with natural language.
-            </p>
+            
           </div>
           <div className="flex flex-col space-y-4 border-t border-gray-200 bg-gray-50 p-7 sm:p-10">
             {examples.map((example, i) => (
@@ -142,6 +104,10 @@ export default function Chat() {
                 {example}
               </button>
             ))}
+          </div>
+          <div className="flex flex-col space-y-4 p-7 sm:p-10">
+            <p>Your documents:</p>
+            
           </div>
         </div>
       )}
