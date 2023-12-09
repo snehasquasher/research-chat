b6ca96ff0167d93ccdcc1cb649cca7d82e7abf75
nijhawananushka <55636931+nijhawananushka@users.noreply.github.com>
2023-12-09 18:07:55 -0500

    Formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,29 +4,14 @@ import '../globals.css';
 import RootLayout from '../components/RootLayout';
 
 function MyApp({ Component, pageProps }: { Component: React.ComponentType; pageProps: any }) {
-  
 
 
-  return (
-    <RootLayout>
-      <Component {...pageProps} />
-    </RootLayout>
-    /* <div>
-      <input type="file" onChange={handleFileChange} />
-      {selectedFile && selectedFile.type === 'application/pdf' && (
-        <div>
-          <h2>PDF Preview:</h2>
-          <embed src={URL.createObjectURL(selectedFile)} type="application/pdf" width="100%" height="500px" />
-        </div>
-      )}
-      {selectedFile && selectedFile.type === 'text/plain' && (
-        <div>
-          <h2>Text File Preview:</h2>
-          <p>{selectedFileText}</p>
-        </div>
-      )}
-    </div>  */
-  );
+
+	return (
+		<RootLayout>
+			<Component {...pageProps} />
+		</RootLayout>
+	);
 }
 
 export default MyApp;
--- pages/upload.tsx
+++ pages/upload.tsx
@@ -3,20 +3,14 @@ import Link from 'next/link'
 import FileUploadForm from '@/components/FileUploadForm'
 
 export default function Upload() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div className="grid  lg:mb-0 lg:grid-cols-1 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <h1 className={`group rounded-lg mb-3 text-4xl font-semibold`}>Upload Research Papers</h1>
-        
-    </div>
-    <div>
-    <FileUploadForm/>
-    </div>
-   
-
-      
-
-      
-    </main>
-  )
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="grid  lg:mb-0 lg:grid-cols-1 relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+				<h1 className={`group rounded-lg mb-3 text-4xl font-semibold`}>Upload Research Papers</h1>
+			</div>
+			<div>
+				<FileUploadForm />
+			</div>
+		</main>
+	)
 }
