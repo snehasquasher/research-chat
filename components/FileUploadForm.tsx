b01c4a772dec3703fe3261a2540b1b1902fa8653
nijhawananushka <55636931+nijhawananushka@users.noreply.github.com>
2023-12-09 18:06:42 -0500

    formatting

--- components/FileUploadForm.tsx
+++ components/FileUploadForm.tsx
@@ -6,69 +6,69 @@ import PDFPreview from "./PDFPreview";
 import classNames from "classnames";
 
 const FileUploadForm = () => {
-  const [files, setFiles] = useState<File[]>([]);
-  const [uploading, setUploading] = useState(false);
-  const handleFileSelected = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      //convert `FileList` to `File[]`
-      const _files = Array.from(e.target.files);
-      setFiles(_files);
-    }
-  };
+	const [files, setFiles] = useState<File[]>([]);
+	const [uploading, setUploading] = useState(false);
+	const handleFileSelected = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.target.files) {
+			//convert `FileList` to `File[]`
+			const _files = Array.from(e.target.files);
+			setFiles(_files);
+		}
+	};
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => { /* removed async */
-    e.preventDefault();
-    
-    const formData = new FormData();
-    files.forEach((file, i) => {
-      formData.append(file.name, file);
-    });
-    var req = fetch('/api/uploadFiles', {
-        method: 'post',
-        body: formData /* or aFile[0]*/
-    }); // returns a promise
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => { /* removed async */
+		e.preventDefault();
 
-    req.then(function(response) {
-        // returns status + response headers
-        // but not yet the body, 
-        // for that call `response[text() || json() || arrayBuffer()]` <-- also promise
-      
-        if (response.ok) {
-          // status code was 200-299
-          response.json().then((data) => {
-            console.log(data);
-            location.href = "/chat" + '?num=' + data;
-          })
-          
-        } else {
-          // status was something else
-        }
-      }, function(error) {
-        console.error('failed due to network error or cross domain')
-      })
-  };
+		const formData = new FormData();
+		files.forEach((file, i) => {
+			formData.append(file.name, file);
+		});
+		var req = fetch('/api/uploadFiles', {
+			method: 'post',
+			body: formData /* or aFile[0]*/
+		}); // returns a promise
 
-  return (
-    <form className="w-full position-fixed"  onSubmit={handleSubmit}>
-    <div className="flex justify-between">
-        <CustomFileSelector
-            accept="application/pdf"
-            onChange={handleFileSelected}
-        />
-        <button
-            type="submit"
-            className={classNames({
-                "bg-violet-50 text-violet-500 hover:bg-violet-100 px-4 py-2 rounded-md":
-                  true,
-                "disabled pointer-events-none opacity-40": uploading,
-              })}
-              disabled={uploading}
-        >Upload</button>
-        </div>
-        <PDFPreview files={files} />
-      
-    </form>
-  );
+		req.then(function (response) {
+			// returns status + response headers
+			// but not yet the body, 
+			// for that call `response[text() || json() || arrayBuffer()]` <-- also promise
+
+			if (response.ok) {
+				// status code was 200-299
+				response.json().then((data) => {
+					console.log(data);
+					location.href = "/chat" + '?num=' + data;
+				})
+
+			} else {
+				// status was something else
+			}
+		}, function (error) {
+			console.error('failed due to network error or cross domain')
+		})
+	};
+
+	return (
+		<form className="w-full position-fixed" onSubmit={handleSubmit}>
+			<div className="flex justify-between">
+				<CustomFileSelector
+					accept="application/pdf"
+					onChange={handleFileSelected}
+				/>
+				<button
+					type="submit"
+					className={classNames({
+						"bg-violet-50 text-violet-500 hover:bg-violet-100 px-4 py-2 rounded-md":
+							true,
+						"disabled pointer-events-none opacity-40": uploading,
+					})}
+					disabled={uploading}
+				>Upload</button>
+			</div>
+			<PDFPreview files={files} />
+
+		</form>
+	);
 };
 
 export default FileUploadForm;
--- components/PDFPreview.tsx
+++ components/PDFPreview.tsx
@@ -1,75 +1,75 @@
 // components/ImagePreview.tsx
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 type Props = {
-   files: File[];
+	files: File[];
 };
 
 const PDFPreview = ({ files }: Props) => {
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
-  const [selectedFileText, setSelectedFileText] = useState('');
+	const [selectedFile, setSelectedFile] = useState<File | null>(null);
+	const [selectedFileText, setSelectedFileText] = useState('');
 
-  const handleUpload = async () => {
-    if (selectedFile) {
-      const formData = new FormData();
-      formData.append('pdf', selectedFile);
+	const handleUpload = async () => {
+		if (selectedFile) {
+			const formData = new FormData();
+			formData.append('pdf', selectedFile);
 
-      try {
-        const response = await fetch('http://127.0.0.1:5328/api/processDocs', {
-          method: 'POST',
-          body: formData,
-        });
+			try {
+				const response = await fetch('http://127.0.0.1:5328/api/processDocs', {
+					method: 'POST',
+					body: formData,
+				});
 
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
+				if (!response.ok) {
+					throw new Error(`Error: ${response.status}`);
+				}
 
-        const data = await response.json();
-        console.log(data);
-        // Handle the response data here
+				const data = await response.json();
+				console.log(data);
+				// Handle the response data here
 
-      } catch (error) {
-        console.error('Error uploading file:', error);
-      }
-    }
-  };
+			} catch (error) {
+				console.error('Error uploading file:', error);
+			}
+		}
+	};
 
-  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      setSelectedFile(event.target.files[0]);
-      if (selectedFile?.type == 'text/plain') {
-        const contents = await selectedFile.text();
-        setSelectedFileText(contents);
+	const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.files && event.target.files.length > 0) {
+			setSelectedFile(event.target.files[0]);
+			if (selectedFile?.type == 'text/plain') {
+				const contents = await selectedFile.text();
+				setSelectedFileText(contents);
 
-        console.log(contents);
-      }
-    }
-  };
+				console.log(contents);
+			}
+		}
+	};
 
-  return (
-    <div>
-      <ul >
-        {files.map((file) => {
-          return (
-            <li key={file.name}>
-               {file.type === 'application/pdf' && (
-                <div>
-                  <h2 className={`group rounded-lg mb-3 font-semibold`}>PDF Preview of {file.name}:</h2>
-                  <embed src={URL.createObjectURL(file)} type="application/pdf" width="100%" height="500px" />
-                </div>
-              )}
-              {file.type === 'text/plain' && (
-                <div>
-                  <h2 className={`group rounded-lg mb-3 font-semibold`}>Text File Preview of {file.name}:</h2>
-                  <embed src={URL.createObjectURL(file)} type="text/plain" width="100%" height="500px" />
-                </div>
-              )}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<ul >
+				{files.map((file) => {
+					return (
+						<li key={file.name}>
+							{file.type === 'application/pdf' && (
+								<div>
+									<h2 className={`group rounded-lg mb-3 font-semibold`}>PDF Preview of {file.name}:</h2>
+									<embed src={URL.createObjectURL(file)} type="application/pdf" width="100%" height="500px" />
+								</div>
+							)}
+							{file.type === 'text/plain' && (
+								<div>
+									<h2 className={`group rounded-lg mb-3 font-semibold`}>Text File Preview of {file.name}:</h2>
+									<embed src={URL.createObjectURL(file)} type="text/plain" width="100%" height="500px" />
+								</div>
+							)}
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default PDFPreview;
