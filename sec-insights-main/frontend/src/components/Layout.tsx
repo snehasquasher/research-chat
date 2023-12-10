4ee5f2482590bc702be0017d3d21c4a14393e5a3
Sneha <sneha.squasher@gmail.com>
2023-12-10 13:22:24 -0500

    wip

--- sec-insights-main/frontend/src/components/Layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import type { PropsWithChildren } from "react";
-const Layout = ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
-};
-export default Layout;
